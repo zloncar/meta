84ccedef50b933c185d0f3d831a8830322342565
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2013-10-23 11:24:16 +0100

    Use recursion for method delegations

--- lib/my_forwardable.rb
+++ lib/my_forwardable.rb
@@ -1,13 +1,15 @@
 module MyForwardable
   def def_delegator(accessor, method, ali=method)
-    p accessor
-    p method
-    define_method(ali) { |*args, &block| send(accessor).send(method, *args, &block) }
+    define_method(ali) do |*args, &block|
+      send(accessor).send(method, *args, &block)
+    end
   end
 
   def def_delegators(accessor, *methods)
-    for method in methods
-      def_delegator(accessor, method)
+    unless methods.size == 0
+      def_delegator(accessor, methods[0])
+      methods.shift
+      def_delegators(accessor, *methods)
     end
   end
 end
