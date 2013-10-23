7a3f4deff9c43c695a31ddd71db3833d8b41be41
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2013-10-23 10:14:39 +0100

    Add block to define method, so it passes.

--- lib/my_forwardable.rb
+++ lib/my_forwardable.rb
@@ -2,7 +2,7 @@ module MyForwardable
   def def_delegator(accessor, method, ali=method)
     p accessor
     p method
-    define_method(ali) { |*args| send(accessor).send(method, *args) }
+    define_method(ali) { |*args, &block| send(accessor).send(method, *args, &block) }
   end
 
   def def_delegators(accessor, *methods)
